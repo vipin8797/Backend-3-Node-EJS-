7e1ea520cad6e42c31efae327b89d6614ed2e86d
vipin yadav <cy7795151@gmail.com>
2025-01-15 13:20:07 +0530

    views dir created & index.ejs created in it.

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const app = express();
+//requring path for ejs files path
+const path = require('path');
 
 
 
-
+//setting views engine for views templates
+app.set('view engin', 'ejs');
+//setting path for views templates
+app.set('views', path.join(__dirname, 'views'));
 
 
 app.get('/', (req, res) => {
     res.send("Home route is working");
+})
+app.get('/index', (req, res) => {
+    res.render('index.ejs');
 })
 
 
